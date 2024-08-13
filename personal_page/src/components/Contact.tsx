36100984c937a78106bec0d31ce967d171ab6383
apandey16 <apande02@calpoly.edu>
2024-08-13 00:23:39 -0700

    links

--- personal_page/src/components/Contact.tsx
+++ personal_page/src/components/Contact.tsx
@@ -14,40 +14,40 @@ const Contact: React.FC = () => {
     console.log("Message:", message);
   };
 
-  return (
+return (
     <section id="contactMe">
-      <h2>Contact Me</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+        <h2>Contact Me</h2>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="message">Message:</label>
+                <textarea
+                    id="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+            </div>
+            <button type="submit" onClick={() => window.location.href = "mailto:ansh.pandey1234@gmail.com"}>Submit</button>
+        </form>
     </section>
-  );
+);
 };
 
 export default Contact;
--- personal_page/src/components/home/links.tsx
+++ personal_page/src/components/home/links.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
+import { MdEmail, MdKeyboardArrowDown } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa";
 
 import "./stylesheet_home.css";
 
@@ -17,6 +18,15 @@ const links = () => {
       <a href="https://github.com/apandey16" target="_blank" rel="noreferrer">
         <BsGithub />
       </a>
+      <a href="https://instagram.com/steelstine" target="_blank" rel="noreferrer">
+        <FaInstagram />
+      </a>
+      <a
+        href="#home/email"
+        onClick={() => window.location.href = "mailto:ansh.pandey1234@gmail.com"}
+      >
+        <MdEmail />
+      </a>
       <a
         href="#contactMe"
         onClick={() =>
@@ -25,7 +35,7 @@ const links = () => {
             ?.scrollIntoView({ behavior: "smooth" })
         }
       >
-        <MdEmail />
+       <MdKeyboardArrowDown /> 
       </a>
     </div>
   );
