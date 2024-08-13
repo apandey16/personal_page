bea56b114bb07182eaf2dfc082914cbb43e3c90f
apandey16 <apande02@calpoly.edu>
2024-08-12 23:57:08 -0700

    about me

--- personal_page/src/components/about/About.tsx
+++ personal_page/src/components/about/About.tsx
@@ -4,10 +4,43 @@ import "./stylesheet_about.css";
 const About: React.FC = () => {
   return (
     <section id="about">
-      <h2>About Me</h2>
-      <div className="container">
-        <p>
-          Hello! I am Ansh Pandey, a software developer based in California.
+      <h2 className="header">About Me</h2>
+      <div className="text-container">
+        <p className="justified-paragrphs">
+          Hello! I am Ansh Pandey, a goal-orientated, dedicated, and ambitious
+          final year student at California Polytechnic State University, San
+          Luis Obispo. I am currently pursuing a Bachelor of Science in Computer
+          Science and a minor in Entrepreneurship. I am a passionate software
+          developer with a strong foundation in computer science and programming
+          principles. I am proficient in Java, Python, C#, and
+          Typescript/JavaScript. I have experience in full-stack web
+          development, mobile app development, AWS, Firebase, and general
+          software engineering principles. I am also familiar with cloud
+          computing, machine learning, and data science. I am a quick learner
+          and a team player who is always eager to learn new technologies and
+          work on challenging projects.
+        </p>
+        <p className="justified-paragrphs">
+          Rooted in a technical foundation, I am enthusiastic about working in
+          settings where I can effectively bridge the gap between innovation and
+          real-world application while solving global challenges. I have been
+          able to do this at work as a Jr. Software Developer at Amazon. My
+          academic journey has equipped me with a solid understanding of
+          programming languages, data structures, algorithmic design, data
+          science, and computational intelligence. I am particularly intrigued
+          by the potential of quantum computing to enhance machine learning
+          techniques and drive advancements in numerous sectors. Additionally, I
+          have been exporessed to the world of entrepreneurship through my minor
+          in Entrepreneurship. I have been able to work on projects in class
+          that have taken me through the process of creating a business plan,
+          pitching to investors, and creating a prototype. I hope to be able to
+          use my technical skills and entrepreneurial mindset to be able to
+          create a postive impact on the world.
+        </p>
+        <p className="justified-paragrphs">
+          I am a firm believer in the power of collaboration and teamwork. Let's
+          connect and explore possibilities to make positive impacts in the
+          ever-evolving landscape of technology and its application!
         </p>
       </div>
     </section>
